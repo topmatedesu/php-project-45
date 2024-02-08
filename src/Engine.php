ca2023cfd53814e6346f01fb4ba89abb3dc75cf8
topmate <1stbagswag@gmail.com>
2024-02-08 17:36:07 +0300

    Fix Engine.php

--- src/Engine.php
+++ src/Engine.php
@@ -5,17 +5,20 @@ namespace Php\Project\Engine;
 use function cli\line;
 use function cli\prompt;
 
-function game(string $task, array $result)
+const ROUNDS_COUNT = 3;
+
+function runGame(string $description, array $gameData)
 {
     line('Welcome to the Brain Games!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
-    line($task);
+    line($description);
 
-    foreach ($result as $key => $value) {
-        $answer = prompt("Question: $key\nYour answer");
-        if ($answer != $value) {
-            line("'$answer' is wrong answer ;(. Correct answer was '$value'.");
+    foreach ($gameData as [$question, $correctAnswer]) {
+        line("Question: $question");
+        $userAnswer = prompt("Your answer");
+        if ($userAnswer != $correctAnswer) {
+            line("'$userAnswer' is wrong answer ;(. Correct answer was '$correctAnswer'.");
             line("Let's try again, %s!", $name);
             return;
         } else {
