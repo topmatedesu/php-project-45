fbc355d055c1038996c61e478bd9d88242c38f87
topmate <1stbagswag@gmail.com>
2024-02-08 21:32:30 +0300

    Fix progression game func

--- src/Games/Progression.php
+++ src/Games/Progression.php
@@ -8,19 +8,27 @@ use const Php\Project\Engine\ROUNDS_COUNT;
 
 const DESCRIPTION = 'What number is missing in the progression?';
 
+function createProgression()
+{
+    $progression = [];
+    $maxElementsCount = 10;
+    $start = rand(1, 100);
+    $step = rand(1, 10);
+
+    for ($i = $start; count($progression) < $maxElementsCount; $i += 1) {
+        $i += $step;
+        $progression[] = $i;
+    }
+
+    return $progression;
+}
+
 function runProgression()
 {
     $gameData = [];
-    $progression = [];
-    $question = [];
-    $correctAnswer = [];
 
     for ($i = 0; $i < ROUNDS_COUNT; $i += 1) {
-        $start = rand(0, 3);
-        $end = rand(20, 50);
-        $step = rand(2, 5);
-
-        $progression = range($start, $end, $step);
+        $progression = createProgression();
         $randNum = array_rand($progression);
         $correctAnswer = $progression[$randNum];
         $progression[$randNum] = '..';
