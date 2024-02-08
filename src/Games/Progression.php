e849c0289250203f32f51e701d336336fb8b37b2
topmate <1stbagswag@gmail.com>
2024-02-08 17:46:20 +0300

    Fix progression game

--- src/Games/Progression.php
+++ src/Games/Progression.php
@@ -2,28 +2,30 @@
 
 namespace Php\Project\Games\Progression;
 
-use function Php\Project\Engine\game;
+use function Php\Project\Engine\runGame;
 
-function progression()
+use const Php\Project\Engine\ROUNDS_COUNT;
+const DESCRIPTION = 'What number is missing in the progression?';
+
+function runProgression()
 {
-    $task = 'What number is missing in the progression?';
-    $result = [];
+    $gameData = [];
     $progression = [];
-    $numbers = [];
-    $answer = [];
+    $question = [];
+    $correctAnswer = [];
 
-    for ($i = 0; $i < 3; $i++) {
+    for ($i = 0; $i < ROUNDS_COUNT; $i += 1) {
         $start = rand(0, 3);
         $end = rand(20, 50);
         $step = rand(2, 5);
 
         $progression = range($start, $end, $step);
         $randNum = array_rand($progression);
-        $answer[] = $progression[$randNum];
+        $correctAnswer = $progression[$randNum];
         $progression[$randNum] = '..';
-        $numbers[$i] = implode(' ', $progression);
+        $question = implode(' ', $progression);
+        $gameData[] = [$question, $correctAnswer];
     }
 
-    $result = array_combine($numbers, $answer);
-    game($task, $result);
+    runGame(DESCRIPTION, $gameData);
 }
