00e4b81d4c65e751f6fb665d71fab5e0048c62e1
topmate <1stbagswag@gmail.com>
2024-02-09 17:25:11 +0300

    Fix gcd func

--- src/Games/Gcd.php
+++ src/Games/Gcd.php
@@ -8,26 +8,28 @@ use const Php\Project\Engine\ROUNDS_COUNT;
 
 const DESCRIPTION = 'Find the greatest common divisor of given numbers.';
 
+function getGcd(int $number1, int $number2)
+{
+    $result = 1;
+
+    for ($i = 1; $i < ($number1 + 1); $i += 1) {
+        if ($number1 % $i === 0 && $number2 % $i === 0) {
+            $result = $i;
+        }
+    }
+
+    return $result;
+}
+
 function runGcd()
 {
     $gameData = [];
-    $question = [];
-    $correctAnswer = [];
 
     for ($i = 0; $i < ROUNDS_COUNT; $i += 1) {
-        $randNum1 = rand(1, 100);
-        $randNum2 = rand(1, 100);
+        $randNum1 = rand(2, 50);
+        $randNum2 = rand(10, 100);
         $question = "{$randNum1} {$randNum2}";
-
-        while ($randNum1 != $randNum2) {
-            if ($randNum1 > $randNum2) {
-                $randNum1 = $randNum1 - $randNum2;
-            } else {
-                $randNum2 = $randNum2 - $randNum1;
-            }
-        }
-
-        $correctAnswer = $randNum2;
+        $correctAnswer = getGcd($randNum1, $randNum2);
         $gameData[] = [$question, $correctAnswer];
     }
 
